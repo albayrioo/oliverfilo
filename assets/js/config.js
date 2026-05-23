932f55157775feb570ee5128a745cc2a26354c83
albayrioo <ozkagsr@gmail.com>
2026-05-24 02:19:20 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,6 @@ window.SITE_CONFIG = {
   phoneDisplay: '905543423514',
   phoneRaw: '905543423514',
   whatsappRaw: '905530752552',
-  whatsappMessage: 'Merhaba, Cordinalyus Rent Filo için araç rezervasyonu yapmak istiyorum.',
+  whatsappMessage: 'Merhaba, Garent Filo Araç Rezervasyonu Yapmak İstiyorum.',
   workingHours: '7/24 rezervasyon ve iletişim desteği'
 };
