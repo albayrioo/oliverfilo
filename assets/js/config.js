49990ee91f36377c962da0f5cf8ad6f9bb6a8cf0
albayrioo <ozkagsr@gmail.com>
2026-05-26 00:31:48 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 window.SITE_CONFIG = {
   companyName: 'GARENT FİLO',
-  phoneDisplay: '05528525655',
-  phoneRaw: '05528525655',
+  phoneDisplay: '905528525655',
+  phoneRaw: '905528525655',
   whatsappRaw: '905528525655',
   whatsappMessage: 'Merhaba, Garent Filo Araç Rezervasyonu Yapmak İstiyorum.',
   workingHours: '7/24 rezervasyon ve iletişim desteği'
